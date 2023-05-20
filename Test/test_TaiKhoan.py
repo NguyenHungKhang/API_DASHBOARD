371e671625570290551bc5fb539f6dd2daeaa091
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2023-05-20 09:19:36 +0700

    Update test_TaiKhoan.py

--- Test/test_TaiKhoan.py
+++ Test/test_TaiKhoan.py
@@ -10,162 +10,134 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
+import connection
 
+ENDPOINT = "http://ec2-3-82-188-235.compute-1.amazonaws.com:8080/"
 
-ENDPOINT = "http://ec2-54-90-106-213.compute-1.amazonaws.com:8080/"
+  
+def test_tCTaiKhoan001():
+  driver = connection.test_connection_driver()
+  driver.maximize_window()
+  wait = WebDriverWait(driver, timeout=10)
 
-class TestTCTaiKhoan001():
-  def setup_method(self, method):
-    options = Options()
-    options.add_argument('--no-sandbox')  
-    self.driver = webdriver.Chrome(chrome_options=options)
-    self.driver.maximize_window()
-    self.wait = WebDriverWait(self.driver, timeout=10)
-    self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_tCTaiKhoan001(self):
-    self.driver.get(ENDPOINT)
-    self.driver.set_window_size(974, 1032)
-    self.driver.find_element(By.LINK_TEXT, "Login").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
-    self.driver.find_element(By.CSS_SELECTOR, "button").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input").click()
+  driver.get(ENDPOINT)
+  driver.set_window_size(974, 1032)
+  driver.find_element(By.LINK_TEXT, "Login").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
+  driver.find_element(By.CSS_SELECTOR, "button").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
+  driver.find_element(By.CSS_SELECTOR, "input").click()
 
-class TestTCTaiKhoan002():
-  def setup_method(self, method):
-    options = Options()
-    options.add_argument('--no-sandbox')  
-    self.driver = webdriver.Chrome(chrome_options=options)
-    self.driver.maximize_window()
-    self.wait = WebDriverWait(self.driver, timeout=10)
-    self.vars = {}
+  driver.quit()
   
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_tCTaiKhoan002(self):
-    self.driver.get(ENDPOINT)
-    self.driver.set_window_size(974, 1032)
-    self.driver.find_element(By.LINK_TEXT, "Login").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
-    self.driver.find_element(By.CSS_SELECTOR, "button").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input").click()
-    self.driver.find_element(By.CSS_SELECTOR, "th > button").click()
-    element = self.driver.find_element(By.CSS_SELECTOR, "th > button")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("nguyenhungkhang321@gmail.com")
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(4)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(4)").send_keys("123")
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(6)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(6)").send_keys("1")
-    self.driver.find_element(By.CSS_SELECTOR, ".modal-body").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".modal-right-button").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".fa-chevron-right > path").click()
-    self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(6) > td:nth-child(3) > p").click()
+def test_tCTaiKhoan002():
+  driver = connection.test_connection_driver()
+  driver.maximize_window()
+  wait = WebDriverWait(driver, timeout=10)
 
-class TestTCTaiKhoan003():
-  def setup_method(self, method):
-    options = Options()
-    options.add_argument('--no-sandbox')  
-    self.driver = webdriver.Chrome(chrome_options=options)
-    self.driver.maximize_window()
-    self.wait = WebDriverWait(self.driver, timeout=10)
-    self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_tCTaiKhoan003(self):
-    self.driver.get(ENDPOINT)
-    self.driver.set_window_size(974, 1032)
-    self.driver.find_element(By.LINK_TEXT, "Login").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
-    self.driver.find_element(By.CSS_SELECTOR, "button").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".fa-chevron-right > path").click()
-    self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(6) > td:nth-child(2) > p").click()
-    self.driver.find_element(By.ID, "inputStatus").click()
-    self.driver.find_element(By.ID, "inputStatus").send_keys("0")
-    self.driver.find_element(By.CSS_SELECTOR, "button:nth-child(3)").click()
+  driver.get(ENDPOINT)
+  driver.set_window_size(974, 1032)
+  driver.find_element(By.LINK_TEXT, "Login").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
+  driver.find_element(By.CSS_SELECTOR, "button").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
+  driver.find_element(By.CSS_SELECTOR, "input").click()
+  driver.find_element(By.CSS_SELECTOR, "th > button").click()
+  element = driver.find_element(By.CSS_SELECTOR, "th > button")
+  actions = ActionChains(driver)
+  actions.move_to_element(element).perform()
+  element = driver.find_element(By.CSS_SELECTOR, "body")
+  actions = ActionChains(driver)
+  actions.move_to_element(element, 0, 0).perform()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("nguyenhungkhang321@gmail.com")
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(4)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(4)").send_keys("123")
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(6)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(6)").send_keys("1")
+  driver.find_element(By.CSS_SELECTOR, ".modal-body").click()
+  driver.find_element(By.CSS_SELECTOR, ".modal-right-button").click()
+  driver.find_element(By.CSS_SELECTOR, ".fa-chevron-right > path").click()
+  driver.find_element(By.CSS_SELECTOR, "tr:nth-child(6) > td:nth-child(3) > p").click()
 
-class TestTCTaiKhoan004():
-  def setup_method(self, method):
-    options = Options()
-    options.add_argument('--no-sandbox')  
-    self.driver = webdriver.Chrome(chrome_options=options)
-    self.driver.maximize_window()
-    self.wait = WebDriverWait(self.driver, timeout=10)
-    self.vars = {}
+  driver.quit()
   
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_tCTaiKhoan004(self):
-    self.driver.get(ENDPOINT)
-    self.driver.set_window_size(974, 1032)
-    self.driver.find_element(By.LINK_TEXT, "Login").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
-    self.driver.find_element(By.CSS_SELECTOR, "button").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".fa-chevron-right").click()
-    self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(6) .svg-inline--fa").click()
+def test_tCTaiKhoan003():
+  driver = connection.test_connection_driver()
+  driver.maximize_window()
+  wait = WebDriverWait(driver, timeout=10)
 
-class TestTCTaiKhoan005():
-  def setup_method(self, method):
-    options = Options()
-    options.add_argument('--no-sandbox')  
-    self.driver = webdriver.Chrome(chrome_options=options)
-    self.driver.maximize_window()
-    self.wait = WebDriverWait(self.driver, timeout=10)
-    self.vars = {}
+  driver.get(ENDPOINT)
+  driver.set_window_size(974, 1032)
+  driver.find_element(By.LINK_TEXT, "Login").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
+  driver.find_element(By.CSS_SELECTOR, "button").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
+  driver.find_element(By.CSS_SELECTOR, "input").click()
+  driver.find_element(By.CSS_SELECTOR, ".fa-chevron-right > path").click()
+  driver.find_element(By.CSS_SELECTOR, "tr:nth-child(6) > td:nth-child(2) > p").click()
+  driver.find_element(By.ID, "inputStatus").click()
+  driver.find_element(By.ID, "inputStatus").send_keys("0")
+  driver.find_element(By.CSS_SELECTOR, "button:nth-child(3)").click()
+
+  driver.quit()
   
-  def teardown_method(self, method):
-    self.driver.quit()
+def test_tCTaiKhoan004():
+  driver = connection.test_connection_driver()
+  driver.maximize_window()
+  wait = WebDriverWait(driver, timeout=10)
+
+  driver.get(ENDPOINT)
+  driver.set_window_size(974, 1032)
+  driver.find_element(By.LINK_TEXT, "Login").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
+  driver.find_element(By.CSS_SELECTOR, "button").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
+  driver.find_element(By.CSS_SELECTOR, "input").click()
+  driver.find_element(By.CSS_SELECTOR, ".fa-chevron-right").click()
+  driver.find_element(By.CSS_SELECTOR, "tr:nth-child(6) .svg-inline--fa").click()
+
+  driver.quit()
   
-  def test_tCTaiKhoan005(self):
-    self.driver.get(ENDPOINT)
-    self.driver.set_window_size(974, 1032)
-    self.driver.find_element(By.LINK_TEXT, "Login").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
-    self.driver.find_element(By.CSS_SELECTOR, "button").click()
-    self.driver.implicitly_wait(3)
-    self.driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input").click()
-    self.driver.find_element(By.CSS_SELECTOR, "input").send_keys("ngh")
-    self.driver.find_element(By.CSS_SELECTOR, "td:nth-child(1)").click()
+def test_tCTaiKhoan005():
+
+  driver = connection.test_connection_driver()
+  driver.maximize_window()
+  wait = WebDriverWait(driver, timeout=10)
+
+  driver.get(ENDPOINT)
+  driver.set_window_size(974, 1032)
+  driver.find_element(By.LINK_TEXT, "Login").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(2)").send_keys("admin@admin.com")
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").click()
+  driver.find_element(By.CSS_SELECTOR, "input:nth-child(3)").send_keys("1")
+  driver.find_element(By.CSS_SELECTOR, "button").click()
+  driver.implicitly_wait(3)
+  driver.find_element(By.CSS_SELECTOR, ".item:nth-child(1) img").click()
+  driver.find_element(By.CSS_SELECTOR, "input").click()
+  driver.find_element(By.CSS_SELECTOR, "input").send_keys("ngh")
+  driver.find_element(By.CSS_SELECTOR, "td:nth-child(1)").click()
+
+  driver.quit()
